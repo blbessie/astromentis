f3620445cb5bb2369d21477a05e20a4cca518e0f
Korhan Akcura <korhan.akcura@sgdigital.com>
2018-10-21 02:26:17 -0400

    Remove tingodb.

--- server.js
+++ server.js
@@ -5,8 +5,8 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var Engine = require('tingodb')();
-var db = new Engine.Db('./db', {});
+//var Engine = require('tingodb')();
+//var db = new Engine.Db('./db', {});
 //var parse = require('querystring').parse;
 
 function collectRequestData(request, callback) {
