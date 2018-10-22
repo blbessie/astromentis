ea3f15668fd7c7194e5771857cee196d1ca24b7f
Harleen <yuning_19@hotmail.com>
2018-10-22 15:08:43 -0400

    Revert "update bio-monitor"
    This reverts commit 5c85d3b7928665adca4dd4017e2db48556b05ea6.

--- bio-monitor-predict.js
+++ bio-monitor-predict.js
@@ -8,27 +8,18 @@ exports.predict = function (input1, input2) {
 	var csv = require('fast-csv');
 
 	var Average_read = [];
-
-
-	
 	csv.fromPath('./public/csv/Average_sensor_reading.csv').on('data', function(data)
 	{
-		//console.log(data);
-		//console.log(data.length);
 		Average_read.push(data);
-		//console.log(Average_read[0].length);
 
 		var BioSpy_prediction_model = [];
 		csv.fromPath('./public/csv/BioSpy_prediction_model.csv').on('data', function(data)
 		{
 			BioSpy_prediction_model.push(data);
 
-			var new_params = rep(99999,30); //= get(new_params)
+			var new_params = [rep(99999,30)]; //= get(new_params)
 			var param_comparison;
-			//console.log(data);
-			//console.log(Average_read[0].length);
-			//console.log(new_params);
-			/*for (var i =0; i < Average_read[0].length; i++)
+			for (var i =0; i < Average_read[0].length; i++)
 			{
 
 				console.log(Average_read[i]);
@@ -55,11 +46,11 @@ exports.predict = function (input1, input2) {
 			{
 				predicted_prob[c] = sum(BioSpy_prediction_model[1][c]);
 				console.log(predicted_prob[c]);
-			} */
+			}
 
 			
 
-		}); 
+		});
 	});
 };
 
